d914f0c31b72325252334e68872ab03913cad17c
AmitMiz5 <amit.mizrahi582@gmail.com>
2024-11-08 12:04:07 +0200

    add toast message

---
+++ Shared/Models/ToastMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace template.Shared.Models
+{
+    public class ToastMessage
+    {
+        public string Message { get; set; }
+        public string Type { get; set; }
+    }
+}
+
+
